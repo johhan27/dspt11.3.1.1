71db6c18448b849bf1edd32289e3f387caddc95f
Johan Sanchez <johhan27@Johans-Air.lan>
2021-05-12 18:01:25 -0700

    name editing

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup
+
+
+setup(
+    name='dspt11_lambdata_johan_sanchez',  # Required
+    version='0.0.1',  # Required
+    author='DSPT11',  # Optional
+    author_email='johan-sanchez@lambdastudents.com',  # Optional
+    keywords='reddit',  # Optional
+    packages=['mymodule'],  # Required
+    python_requires='>=3.6, <4',
+    install_requires=['pandas'],  # Optional
+)
